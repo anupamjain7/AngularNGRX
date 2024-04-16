604a4d5323e6532104079294abecc24ba6f1f76b
Anupam Jain <nextgenboy14@gmail.com>
2024-04-16 19:43:34 +0530

    Counter with NGRX implemented

--- AngularNGRX/src/app/app.config.ts
+++ AngularNGRX/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 import { provideClientHydration } from '@angular/platform-browser';
-
+import { reducers, metaReducers} from './reducers'
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideAnimations(), provideClientHydration(),
+    provideStore(reducers, {metaReducers})
+  ]
 };
